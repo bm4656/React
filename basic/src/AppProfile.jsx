b50d1b405f4fbfb0ae783e61add0aa75373b3889
bm4656 <newjin46@gmail.com>
2023-01-13 20:56:21 +0900

    feat: add Profile 'new' props

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -5,11 +5,13 @@ const AppProfile = () => {
   return (
     <div>
       <Profile
+        isNew='true'
         img='https://images.unsplash.com/photo-1669199145926-7dad570f3c3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
         name='annie'
         title='프론트엔드 개발자'
       />
       <Profile
+        isNew='true'
         img='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
         name='Sue'
         title='풀스택 개발자'
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-export default function Profile({ img, name, title }) {
+export default function Profile({ img, name, title, isNew }) {
   return (
     <div className='profile'>
       <img className='photo' src={img} alt='avatar' />
+      {isNew && <span className='new'>new</span>}
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
