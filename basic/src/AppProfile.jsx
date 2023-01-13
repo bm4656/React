9916af6316cf27e6f2acfdfaa5b5137a80069840
bm4656 <newjin46@gmail.com>
2023-01-13 20:34:24 +0900

    feat: Completing Profile Component

--- basic/src/AppProfile.jsx
+++ basic/src/AppProfile.jsx
@@ -4,7 +4,21 @@ import './App.css';
 const AppProfile = () => {
   return (
     <div>
-      <Profile />
+      <Profile
+        img='https://images.unsplash.com/photo-1669199145926-7dad570f3c3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
+        name='annie'
+        title='프론트엔드 개발자'
+      />
+      <Profile
+        img='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+        name='Sue'
+        title='풀스택 개발자'
+      />
+      <Profile
+        img='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fCVFRCU5NCU4NCVFQiVBMSU5QyVFRCU5NSU4NHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+        name='Jane'
+        title='백엔드 개발자'
+      />
     </div>
   );
 };
--- basic/src/components/Profile.jsx
+++ basic/src/components/Profile.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-export default function Profile() {
+export default function Profile({ img, name, title }) {
   return (
     <div className='profile'>
-      <img
-        className='photo'
-        src='https://images.unsplash.com/photo-1669199145926-7dad570f3c3f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80'
-        alt='avatar'
-      />
-      <h1>Profile</h1>
-      <p>FE 개발자</p>
+      <img className='photo' src={img} alt='avatar' />
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
