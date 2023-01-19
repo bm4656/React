72c7d3f956b3e1d109066744113c9289e044bb12
bm4656 <newjin46@gmail.com>
2023-01-20 00:42:00 +0900

    커스텀 훅 만들기

--- basic/src/basic/components/Products.jsx
+++ basic/src/basic/components/Products.jsx
@@ -1,29 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import useProducts from '../../hooks/use-products';
 
 export default function Products() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState();
-
-  const [count, setCount] = useState(0);
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+  const [loading, error, products] = useProducts({ salesOnly: checked });
   const handleChange = () => setChecked(prev => !prev);
-
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-    fetch(`data`)
-      .then(res => res.json())
-      .then(data => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
-      })
-      .catch(err => setError('에러 발생!'))
-      .finally(() => setLoading(false));
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
-  }, [checked]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -46,7 +27,6 @@ export default function Products() {
           </li>
         ))}
       </ul>
-      <button onClick={() => setCount(prev => prev + 1)}>{count}</button>
     </>
   );
 }
