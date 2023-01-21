cdf86805946a7091c4064e0d6f4a69636904ac64
bm4656 <newjin46@gmail.com>
2023-01-22 01:56:42 +0900

    feat:  헤더 버튼으로 필터링

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import TodoList from './components/TodoList/TodoList';
+import Header from './Header/Header';
+
+const filters = ['all', 'active', 'completed'];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <div>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   );
 }
