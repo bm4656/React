354d51e9a75e111a62576c0db79ca650190ed0bc
bm4656 <newjin46@gmail.com>
2023-07-12 18:59:32 +0900

    feat: 기본 fetch 로 데이터 불러오기

--- tanstack-query/src/components/MusicList.tsx
+++ tanstack-query/src/components/MusicList.tsx
@@ -1,3 +1,35 @@
+import { useEffect, useState } from 'react';
+
+type Music = { title: string; singer: string; id: string };
 export default function MusicList() {
-  return <ul>뮤직리스트 짜라락</ul>;
+  const [musics, setMusics] = useState<Music[]>();
+  const getMuscis = async () => {
+    await fetch(`data/musics.json`)
+      .then((res) => res.json())
+      .then((data: Music[]) => {
+        console.log('데이터 받아옴');
+        setMusics(data);
+      });
+  };
+  useEffect(() => {
+    console.log('fetching....');
+    getMuscis().catch((e) => console.log('에러발생!', e));
+  }, []);
+  return (
+    <>
+      <h2>MUSIC LIST</h2>
+      <ul>
+        {musics &&
+          musics.map((item) => (
+            <li key={item.id}>
+              <article>
+                <p>
+                  {item.title} - {item.singer}
+                </p>
+              </article>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
